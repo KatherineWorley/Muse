ac70f6a0827044599477fcd50ae2fbc7887c642e
Katherine Worley <kdesign216@yahoo.com>
2018-06-08 09:28:19 -0400

    updated update and destroy in posts controler added link to on show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,9 +25,16 @@ class PostsController < ApplicationController
 	end 
 
 	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else 
+			render 'edit'
+		end 
 	end 
 
 	def destroy 
+		@post.destroy
+		redirect_to root_path
 	end 
 
 
