cffd965cef936eee1530e0f188bdc04efd6ed2cb
Katherine Worley <kdesign216@yahoo.com>
2018-06-08 09:20:21 -0400

    looped through posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	def index
+		@posts = Post.all.order("created_at DESC")
 	end
 
 	def show
@@ -32,6 +34,7 @@ class PostsController < ApplicationController
 	private 
 
 	def find_post
+		@post = Post.find(params[:id])
 	end
 
 	def post_params
