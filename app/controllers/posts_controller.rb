b5712f18309d95f71fe6c8f1ff87a2744af80ccf
Katherine Worley <kdesign216@yahoo.com>
2018-06-08 08:59:22 -0400

    added post parmas and create to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,39 @@ class PostsController < ApplicationController
 	def index
 	end
 
-	def new
-		
+	def show
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def create 
+		@post = Post.new(post_params)
+
+		if @post.save 
+			redirect_to @post 
+		else 
+			render 'new'
+		end
+	end 
+
+	def edit 
+	end 
+
+	def update
+	end 
+
+	def destroy 
+	end 
+
+
+	private 
+
+	def find_post
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :link, :description)
+	end 
 end
