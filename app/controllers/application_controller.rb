e0b9d8aa613f085d02cf15f3e1bd9ada9c118003
Katherine Worley <kdesign216@yahoo.com>
2018-06-11 09:01:46 -0400

    updating application controller with permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
-	before_filter :configure_permitted_parameters, if: :devise_controller?
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	protected 
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.for(:sign_up) << :name
-		devise_parameter_sanitizer.for(:account_update) << :name 
+		devise_parameter_sanitizer.permit(:sign_up) << :name
+		devise_parameter_sanitizer.permit(:account_update) << :name 
 	end
+
+	  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
