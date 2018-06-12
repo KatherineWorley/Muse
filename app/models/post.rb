e8a808ee179f20086a96b2da9a16c1161c86281f
Katherine Worley <kdesign216@yahoo.com>
2018-06-12 07:10:57 -0400

    adding image to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
 	belongs_to :user
+	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
