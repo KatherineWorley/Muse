9154166b9e2d9d5a9fcf00a0579bc2f47b84495a
Katherine Worley <kdesign216@yahoo.com>
2018-06-12 07:36:36 -0400

    added comment model for comments on images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_12_110954) do
+ActiveRecord::Schema.define(version: 2018_06_12_113609) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
