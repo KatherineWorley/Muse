1b2072fda4bf356894d778c2b0f7357121b9917a
Katherine Worley <kdesign216@yahoo.com>
2018-06-12 07:41:23 -0400

    adding comments to user and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+	has_many :comments
 end
