9442ca75b5c0901ce532e974f01b34ac864126da
Katherine Worley <kdesign216@yahoo.com>
2018-06-16 10:02:24 -0400

    added up and down methods to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
@@ -34,6 +34,16 @@ class PostsController < ApplicationController
 		else 
 			render 'edit'
 		end 
+	end 
+
+	def upvote
+		@post.upvote_by current_user
+		redirect_to :back
+	end 
+
+	def downvote
+		@post.downvote_by current_user
+		redirect_to :back
 	end 
 
 	def destroy 
