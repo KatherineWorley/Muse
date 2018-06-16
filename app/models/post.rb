970a834c87006f4644afef2d31028365a275d307
Katherine Worley <kdesign216@yahoo.com>
2018-06-16 09:45:40 -0400

    added acts as votable to posts model and updated member block in routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
+	acts_as_votable
 	belongs_to :user
+	has_many :comments
 	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
-	has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts do 
+  	member do 
+  		put "like", to: "posts#vote"
+  		put "dislike", to: "post#upvote"
+  	end
   	resources :comments
   end
   
